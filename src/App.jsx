1901f35ce28fd2aa5f8adea45aaec71f98b248ae
rishaw <rishawraj0703@gmail.com>
2022-12-18 14:22:18 +0530

    general info functinality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from "react";
+import profile from "./assets/profile.jpg";
 import "./App.css";
 
 // todo form
@@ -9,16 +10,29 @@ function App() {
   const [name, setName] = useState("John Doe");
   const [email, setEmail] = useState("john123@gmail.com");
   const [phone, setPhone] = useState("123-456-7890");
+  const [image, setImage] = useState(profile);
 
   const inputName = useRef(null);
   const inputEmail = useRef(null);
   const inputPhone = useRef(null);
+  const inputImage = useRef(null);
 
   function setForm(e) {
     e.preventDefault();
     inputName.current.value = "";
     inputEmail.current.value = "";
     inputPhone.current.value = "";
+    inputImage.current.value = "";
+    let form = document.querySelector(".form");
+    form.classList.add("hide");
+  }
+  function showForm() {
+    let form = document.querySelector(".form");
+    form.classList.remove("hide");
+  }
+
+  function handleImage(e) {
+    setImage(URL.createObjectURL(e.target.files[0]));
   }
 
   return (
@@ -26,7 +40,7 @@ function App() {
       <h1>CV Application</h1>
       {/* ================= contianer ========================= */}
       <div className="container">
-        <div className="form" onSubmit={setForm}>
+        <div className="form no-print" onSubmit={setForm}>
           <h2>form</h2>
           <form>
             <label htmlFor="Name">Name</label> <br />
@@ -59,15 +73,26 @@ function App() {
               onChange={(e) => setPhone(e.target.value)}
             />
             <br />
+            <label htmlFor="image">upload image</label> <br />
+            <input type="file" onChange={handleImage} ref={inputImage} />
+            <br />
             <br />
             <button type="submit">submit</button>
           </form>
         </div>
         {/* ================= preview ========================= */}
         <div className="preview">
+          <button className="no-print" onClick={showForm}>
+            edit
+          </button>
           <h2>{name}</h2>
           <p>{email}</p>
           <p>{phone}</p>
+          <img src={image} alt="profile" height="150px" width="auto" />
+          <br />
+          <button className="no-print" onClick={(e) => print()}>
+            print
+          </button>
         </div>
         {/* ================= preview ========================= */}
       </div>
